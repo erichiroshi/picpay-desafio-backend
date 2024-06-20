966bb4ede04bb3ddb91e78753138a90949a3c2d2
erichiroshi <erichiroshi@gmail.com>
2024-06-20 18:10:05 -0400

    Projeto criado

---
+++ backend/picpay/src/test/java/br/com/erichiroshi/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
